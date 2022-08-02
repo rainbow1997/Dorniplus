c2fc614b7cd41b70b2418f5d81d897972297c13d
Mostafa Jamali <personal@mostafajamali.ir>
2022-08-02 21:19:06 +0430

    solve database seeding problems

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,11 +15,21 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name(),
+            'fname' => $this->faker->name(),
+            'lname' => $this->faker->name(),
+
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'national_code' => rand(117655443,99999999999999999),
+
+            'phone' => rand(117655443,99999999999999999),
+            'birth' => now(),
+            'gender'=> ( rand(0,9) < 5 ) ? 'male':'female',
+            'province_id' =>1,
+            'city_id' => 1,
+            'username' => Str::random(8),
         ];
     }
 
