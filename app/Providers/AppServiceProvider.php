75e6a8d0acdff12722a77065460a4c4cc3e9736e
Mostafa Jamali <personal@mostafajamali.ir>
2022-12-11 11:31:43 +0330

    Solve temporarily  too many redirects problem

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,9 +22,10 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot(){
-        if($this->app->environment('production')) {
+    /*    if($this->app->environment('production')) {
             \URL::forceScheme('https');
         }
     }
+    */
 
 }
